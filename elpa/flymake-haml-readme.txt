d89394f258d6fd7d1a11c11b191f5029eab4f2e1
Alex Sharp <ajsharp@gmail.com>
2013-07-29 20:24:47 -0700

    mass elpa dir update

---
+++ elpa/flymake-haml-readme.txt
@@ -0,0 +1,5 @@
+Usage:
+  (require 'flymake-haml)
+  (add-hook 'haml-mode-hook 'flymake-haml-load)
+
+Uses flymake-easy, from https://github.com/purcell/flymake-easy
---
+++ elpa/smex-20130707.1255/smex-autoloads.el
@@ -0,0 +1,33 @@
+;;; smex-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads (smex-initialize smex) "smex" "smex.el" (20981
+;;;;;;  28797))
+;;; Generated autoloads from smex.el
+
+(autoload 'smex "smex" "\
+
+
+\(fn)" t nil)
+
+(autoload 'smex-initialize "smex" "\
+
+
+\(fn)" t nil)
+
+;;;***
+
+;;;### (autoloads nil nil ("smex-pkg.el") (20981 28797 242601))
+
+;;;***
+
+(provide 'smex-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; smex-autoloads.el ends here
